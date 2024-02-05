f24ad3f5c7482f64255861e6a49264111f02b542
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-02-05 21:11:18 +0200

    add binary_heap

---
+++ binary_heap.py
@@ -0,0 +1,67 @@
+import uuid
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import heapq
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+
+def build_heap_tree(values):
+    heap = [Node(value) for value in values]
+    heapq.heapify(heap)
+    
+    while len(heap) > 1:
+        min1 = heapq.heappop(heap)
+        min2 = heapq.heappop(heap)
+        
+        merged_node = Node(min1.val + min2.val)
+        merged_node.left = min1
+        merged_node.right = min2
+        
+        heapq.heappush(heap, merged_node)
+    
+    return merged_node
+
+heap_values = [4, 8, 2, 5, 9, 1, 6]
+heap_root = build_heap_tree(heap_values)
+
+if __name__ == "__main__":
+    draw_tree(heap_root)
